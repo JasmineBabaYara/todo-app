97527dd5a9dcee063e8b8d3b4988b3345e5babc8
JasmineBabaYara <jassyprincess1@gmail.com>
2021-09-15 14:42:31 -0700

    todo-app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react' 
+import './index.css'
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  constructor(){
+    super()
+    this.state={
+      todo:'',
+      Arr:[]
+    }
+    this.hanldeChange= this.hanldeChange.bind(this);
+    this.handleClick=this.handleClick.bind(this)
+  }
+  hanldeChange(event){
+    this.setState({
+      [event.target.name]:event.target.value
+    })
+  } 
+
+    handleClick(){
+      this.setState({
+        Arr:[...this.state.Arr,this.state.todo]
+      })
+    }
+  
+  render(){
+    return(
+      <div>
+        <input 
+        type='text' 
+        name='todo' 
+        onChange={this.hanldeChange} 
+        value={this.state.todo} />
+        <button onClick={this.handleClick} >Add todo</button>
+        
+        {this.state.Arr.map((item) =>{
+          return(
+            <p>{item}
+              <button>
+                <i class="bi bi-check"></i>
+              </button>
+              <button>
+                <i class="bi bi-trash"></i>
+              </button>
+            </p>
+          )
+        })}
+      </div>
+    )
+  }
 }
 
 export default App;
